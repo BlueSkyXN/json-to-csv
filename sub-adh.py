4c7c18e1b7c5c0e2eb090434cd886365e806f40a
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-05-12 15:47:43 +0800

    20230512-1547

--- sub-adh.py
+++ sub-adh.py
@@ -9,19 +9,15 @@ parser = argparse.ArgumentParser(description='Split text file into smaller chunk
 # 添加输入文件路径参数
 parser.add_argument('-i', '--input', type=str, default=os.path.expanduser("~/Desktop/input.json"), required=False, help='Path to input file')
 
-# 添加输出文件路径参数
-parser.add_argument('-o', '--output', type=str, default=os.path.expanduser("~/Desktop/output.csv"), required=False, help='Path to output CSV file')
+# 添加输出目录路径参数
+parser.add_argument('-o', '--output_dir', type=str, default=os.path.expanduser("~/Desktop/output/"), required=False, help='Path to output directory')
 
 # 添加切割参数
 parser.add_argument('-s', '--chunk_size', type=int, default=200000, help='Chunk size (default: 200000)')
 
 # 解析命令行参数
 args = parser.parse_args()
-print(args.input, args.output)
-
-# 输出目录路径和文件名分开处理，并扩展波浪线
-output_dir = os.path.expanduser(args.output)
-output_file_name = "output.csv"
+print(args.input, args.output_dir)
 
 # 每个小文件的大小（以行数为单位）
 chunk_size = args.chunk_size
@@ -30,15 +26,16 @@ chunk_size = args.chunk_size
 input_file_size = os.path.getsize(args.input)
 
 # 创建输出目录，如果目录不存在
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+if not os.path.exists(args.output_dir):
+    os.makedirs(args.output_dir)
 
 # 切割大文件
 with open(args.input) as f:
     i = 0
     while True:
         # 确定输出文件路径
-        output_path = os.path.join(output_dir, f"output_{i}.txt")
+        output_path = os.path.join(args.output_dir, f"output_{i}.txt")
+        
         # 将块保存为文本文件
         with open(output_path, 'w') as f_out:
             data = []
@@ -46,31 +43,37 @@ with open(args.input) as f:
                 if j >= chunk_size:
                     break
                 data.append(line.strip())
-            f_out.write('\n'.join(data))
-        
-        print(f"{output_path} ({os.path.getsize(output_path) / (1024 * 1024):.2f} MB)")
+
+            # 如果当前块不为空，则保存文本文件并转换为CSV格式
+            if len(data) > 0:
+                f_out.write('\n'.join(data))
+                
+                # 转换为CSV格式并将结果保存到输出目录
+                with open(output_path) as f_in:
+                    df = pd.read_json(f_in, lines=True)
+                
+                # 如果当前文件中包含有效数据，则保存为CSV文件
+                if len(df) > 0:
+                    abs_output_path = os.path.abspath(os.path.join(args.output_dir, f"output_{i}.csv"))
+                    with open(abs_output_path, 'w') as f_out:
+                        df.to_csv(f_out, index=False)
+
+                    print(f"{abs_output_path} ({os.path.getsize(abs_output_path) / (1024 * 1024):.2f} MB)")
+                    
+                else:
+                    print(f"{output_path} is empty, removing file")
+                    os.remove(output_path)
+            
+            # 如果当前块为空，则删除该文件
+            else:
+                print(f"{output_path} is empty, removing file")
+                os.remove(output_path)
+
         i += 1
 
         # 如果已经读到了文件的末尾，则退出循环
         if not line:
             break
 
-# 对每个小文件进行转换为CSV格式的操作
-for j in range(0, i):
-    # 拼接当前小文件的名称
-    input_file_path = os.path.join(output_dir, f"output_{j}.txt")
-    output_file_path = os.path.join(output_dir, output_file_name)
-
-    # 读取txt文件并转换成DataFrame对象
-    df = pd.read_csv(input_file_path, sep=" ")
-
-    # 在循环中逐行读取数据并添加到DataFrame中
-    for _, row in tqdm(df.iterrows(), total=len(df), desc=f"Converting file {j+1}/{i}"):
-        pass
-
-    # 将数据保存为CSV文件
-    abs_output_path = os.path.abspath(output_file_path)
-    df.to_csv(abs_output_path, index=False)
-
-# 打印输出路径
-print(f"\n已将输入文件成功转换并保存到文件：{output_file_path}")
+# 打印输出目录路径
+print(f"\n已将输入文件成功转换并保存到目录：{args.output_dir}")
