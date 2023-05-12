cc7877ad78151fe9d7c66d65ff4a52d29de299d0
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-05-12 11:14:39 +0800

    更新 jtc.py

--- jtc.py
+++ jtc.py
@@ -1,23 +1,41 @@
+import argparse
 import pandas as pd
 import json
-import argparse
+import os
 
-# 创建命令行参数解析器
-parser = argparse.ArgumentParser(description='Convert JSON data to CSV format')
-parser.add_argument('-i', '--input', type=str, required=True, help='Path to input JSON file')
-parser.add_argument('-o', '--output', type=str, required=True, help='Path to output CSV file')
+# 创建一个ArgumentParser对象
+parser = argparse.ArgumentParser(description='Convert JSON query log to CSV format')
+print(f'debug0')
+
+# 添加输入文件路径参数
+parser.add_argument('-i', '--input', type=str, default='querylog.json',required=False, help='Path to input JSON file')
+
+print(f'debug1')
+
+# 添加输出文件路径参数
+parser.add_argument('-o', '--output', type=str, default='large_data.csv',required=False, help='Path to output CSV file')
+
+print(f'debug2')
 
 # 解析命令行参数
 args = parser.parse_args()
+print(f'debug3')
+print(args.input, args.output)
 
-# 读取JSON文件
+
+# 读取JSON文件并处理
 with open(args.input, 'r') as f:
-    data = json.load(f)
+    content = f.read().replace('\n', ',')
+    json_content = f'[{content[:-1]}]'
 
-# 将JSON转换为DataFrame对象
-df = pd.json_normalize(data, 'logs')
+data = json.loads(json_content)
+
+# 将JSON转换成DataFrame对象
+df = pd.DataFrame(data)
 
 # 将数据保存为CSV文件
-df.to_csv(args.output, index=False)
+abs_output_path = os.path.abspath(args.output)
+df.to_csv(abs_output_path, index=False)
 
-print(f'Successfully converted {args.input} to {args.output}')
+# 输出保存路径
+print(f'已将JSON数据成功转换并保存到文件：{abs_output_path}')
