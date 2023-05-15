dd73ff0f81394d02c24845688b6bcabaf0e1b7d6
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-05-15 16:02:32 +0800

    20230515-1602

--- jtc-adh-cmd-plus2.py
+++ jtc-adh-cmd-plus2.py
@@ -1,34 +1,30 @@
-import pandas as pd
 import ijson
-import os
-from tqdm import tqdm
+import json
+import pandas as pd
 import argparse
+import os
 
-# 创建一个ArgumentParser对象
-parser = argparse.ArgumentParser(description='Convert JSON query log to CSV format')
-
+# 创建参数解析器
+parser = argparse.ArgumentParser(description='Convert JSON file to CSV file')
 # 添加输入文件路径参数
 parser.add_argument('-i', '--input', type=str, default=os.path.expanduser("~/Desktop/input.json") ,required=False, help='Path to input JSON file')
-
 # 添加输出文件路径参数
 parser.add_argument('-o', '--output', type=str, default=os.path.expanduser("~/Desktop/output.csv") ,required=False, help='Path to output CSV file')
 
-# 解析命令行参数
 args = parser.parse_args()
-print(args.input, args.output)
 
-# 使用ijson逐步解析JSON文件并转换为DataFrame
-with open(args.input, 'rb') as f:
-    # 用列表推导式替换每行末尾的换行符
-    objects = [obj.rstrip().replace(b'\n', b',') + b'}' for obj in f if obj.strip() != b'']
-    df = pd.read_json(b'[' + b','.join(objects) + b']')
+data = []
+with open(args.input, 'r') as f:
+    for line in f:
+        try:
+            obj = json.loads(line.strip())
+            data.append(obj)
+        except Exception as e:
+            print(f'Error occurred: {e}')
 
-# 在循环中逐行读取数据并添加到DataFrame中
-for _, row in tqdm(df.iterrows(), total=len(df), desc="Converting"):
-    pass
-# 将数据保存为CSV文件
+df = pd.DataFrame(data)
+
+# 将DataFrame写入CSV文件
 abs_output_path = os.path.abspath(args.output)
 df.to_csv(abs_output_path, index=False)
-
-# 打印输出路径
-print(f"\n已将JSON数据成功转换并保存到文件：{abs_output_path}")
+print(f'Successfully converted {args.input} to {abs_output_path}')
--- jtc-adh-cmd-plus3.py
+++ jtc-adh-cmd-plus3.py
@@ -1,7 +1,9 @@
 import ijson
+import json
 import pandas as pd
 import os
 import argparse
+
 
 # 创建ArgumentParser对象
 parser = argparse.ArgumentParser(description='Convert large JSON file to CSV format')
@@ -19,20 +21,18 @@ args = parser.parse_args()
 
 def parse_json(input_path, output_path):
     # 打开输入文件和输出文件
-    with open(input_path, 'r') as input_file, open(output_path, 'w') as output_file:
-        # 逐行读取JSON数据
-        for line in input_file:
-            # 将每行数据转换为json对象
-            obj = json.loads(line.strip())
-            # 将json对象以标准格式写回输出文件，并添加换行符
-            output_file.write(json.dumps(obj) + '\n')
+    with open(input_path, 'r') as input_file:
+        # 读取所有行并组成一个列表
+        lines = input_file.readlines()
 
-    # 使用ijson解析JSON文件并转换为DataFrame对象
-    with open(output_path, 'r') as input_file:
-        # 逐行读取JSON数据
-        objects = ijson.items(input_file, 'item')
-        # 将JSON数据转换为DataFrame对象
-        df = pd.DataFrame(objects)
+        # 将列表中的所有行连接成一个字符串（去掉回车符）
+        data_str = ''.join(lines).replace('\n', '')
+
+        # 解析JSON数组
+        objects = json.loads('[' + data_str + ']')
+
+    # 将JSON数据转换为DataFrame对象
+    df = pd.DataFrame(objects)
 
     # 将数据保存为CSV文件
     abs_output_path = os.path.abspath(output_path)
@@ -40,3 +40,10 @@ def parse_json(input_path, output_path):
     df.to_csv(csv_path, index=False)
 
     print(f'已将JSON数据成功转换并保存到文件：{csv_path}')
+
+if __name__ == '__main__':
+    # 解析命令行参数
+    args = parser.parse_args()
+
+    # 调用 parse_json 函数并传递输入文件路径和输出文件路径
+    parse_json(args.input, args.output)
