b6b83505e1b893531d1be5aea81e8ec3828a8346
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-05-16 16:41:48 +0800

    1.4.2

--- jtc-adh-cmd-ultra-vaex.py
+++ jtc-adh-cmd-ultra-vaex.py
@@ -5,11 +5,11 @@ import os
 parser = argparse.ArgumentParser(description='Convert JSON file to CSV file')
 parser.add_argument('-i', '--input', type=str, default=os.path.expanduser("~/Desktop/input.json"), required=False, help='Path to input JSON file')
 parser.add_argument('-o', '--output', type=str, default=os.path.expanduser("~/Desktop/output.csv"), required=False, help='Path to output CSV file')
-parser.add_argument('-s', '--chunksize', type=int, default=10000, required=False, help='Number of lines to process at a time')
+parser.add_argument('-s', '--chunk_size', type=int, default=10000, required=False, help='Number of lines to process at a time')
 args = parser.parse_args()
 
-chunksize = args.chunksize
-df = vaex.from_json(args.input, chunk_size=chunksize)
+chunk_size = args.chunk_size
+df = vaex.from_json(args.input, chunk_size=chunk_size)
 
 abs_output_path = os.path.abspath(args.output)
 if os.path.isfile(abs_output_path):
